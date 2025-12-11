432682b4525f0b5c672ab88954ec0a1dd3ab4c24
Neelayjain02 <141833494+Neelayjain02@users.noreply.github.com>
2025-12-11 13:57:19 +0530

    Add files via upload

---
+++ app/lnt_report/page.js
@@ -0,0 +1,7 @@
+// 1. Import the component (You can name it whatever you want here, but must be Capitalized)
+import LnTReport from "../../components/lnt_report";
+
+export default function Page() {
+  // 2. Use it with an UPPERCASE letter
+  return <LnTReport />;
+}
