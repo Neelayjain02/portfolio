0b9b967b10bfa4421a9549419bcfa4dc3938f925
Neelayjain02 <141833494+Neelayjain02@users.noreply.github.com>
2025-12-11 14:52:32 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V43VPC5GW4"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V43VPC5GW4');
+</script>
+      </head>
+
       <body className="bg-[#080A10] text-[#E0E4EB]">
         <Navbar />
         {children}
